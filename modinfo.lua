aa46b96466f834bb84e0aff0aa06b1fde5d862ef
ThemInspectors <ThemInspectors@users.noreply.github.com>
2017-05-20 21:46:13 -0600

    Update modinfo.lua
    - Added configuration for later.

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ name = "Desert Enhancement"
 description = "Enhance your deserts, Config coming later."
 
 author = "ThemInspectors"
-version = "1.0"
+version = "2.0"
 forumthread = ""
 api_version = 10
 
@@ -13,4 +13,26 @@ client_only_mod = false
 dst_compatible = true
 server_filter_tags = {}
 
-configuration_options = {}
+configuration_options = {
+  {
+		name = "desutil",
+		label = "Utility Deserts",
+			hover = "Spawn Deserts such as the ""Magical"", ""Bee"" and ""Pig King"" Deserts?",
+		options =	{
+						{description = "Disabled", data = false, hover = "Doesn't Spawn Utility Deserts!"},
+            {description = "Enabled", data = true, hover = "Spawns Utility Deserts!"},
+					},
+		default = false,
+	},
+  {
+  name = "desopt"
+  label = "Optional Deserts"
+    hover = "How many optional deserts to spawn.\ncurrently only works for Desert-Only Worlds."
+  options = {
+      {description = "1", data = 1, hover = "Spawns 1 Optional Desert!"},
+      {description = "2", data = 2, hover = "Spawns 2 Optional Deserts!"},
+      {description = "3", data = 3, hover = "Spawns 2 Optional Deserts!"},
+      
+    }
+  },
+}

221bea4794a9c5232cead98a368f39732848b3e6
ThemInspectors <ThemInspectors@users.noreply.github.com>
2017-05-20 21:24:22 -0600

    Add modinfo.lua
    going to be implementing config!

---
+++ modinfo.lua
@@ -0,0 +1,16 @@
+name = "Desert Enhancement"
+description = "Enhance your deserts, Config coming later."
+
+author = "ThemInspectors"
+version = "1.0"
+forumthread = ""
+api_version = 10
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+all_clients_require_mod = false
+client_only_mod = false
+dst_compatible = true
+server_filter_tags = {}
+
+configuration_options = {}
